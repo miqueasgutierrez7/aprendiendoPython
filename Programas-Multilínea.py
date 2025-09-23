f001843f3cf176508879156bd983c60fa3dc6188
miqueasgutierrez <miqueasmusic@gmail.com>
2025-09-22 22:12:10 -0500

    Tipos de datos básicos

---
+++ Programas-Multilínea.py
@@ -0,0 +1,61 @@
+Programas multilínea
+
+Esperamos que hayas aprendido a escribir programas sencillos de una sola línea en Python que imprimen texto . Sin embargo, los programas reales contienen muchas líneas: desde decenas o cientos de líneas para scripts pequeños hasta miles o incluso más para proyectos grandes. Por lo tanto, en esta lección, escribirás programas que imprimen varias líneas.
+
+Formas de imprimir múltiples líneas
+Consideremos un ejemplo. El siguiente código imprime exactamente tres cadenas, cada una en una nueva línea:
+
+print("I")
+print("know")
+print("Python")
+
+Puede ejecutar este ejemplo utilizando este sitio web o localmente si ha instalado Python en su computadora.
+
+Hay maneras de imprimir el texto anterior con una sola llamada de función. Las analizaremos en los siguientes temas.
+
+La printfunción también le permite imprimir una línea vacía sin ninguna cadena especificada:
+
+print("I")
+print()
+print("know")
+print("")
+print("Python")
+
+¡Tenga en cuenta esto print()y print("")obtendrá el mismo resultado!
+
+Sin embargo, omitir una línea en su código no tendrá ningún efecto.
+
+print("And")
+
+print("you?")
+
+
+#Ejercicios practicos
+
+#1
+
+print("hello")
+print("")
+print()
+
+print("bye")
+
+#2
+
+print(" 1 ")
+print(" 11 ")
+print(" 111 ")
+
+# 3
+
+name = input("Please enter you name:")
+greeting = "Hello," + name + "! Welcome to Python programming."
+print(greeting)
+
+#4
+
+print('_');
+print('<(.)');
+print('(___)');
+print('^ ^');
+
